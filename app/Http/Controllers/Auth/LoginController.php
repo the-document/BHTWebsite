a2e133011bed8113536445e71fc7bdb2762fcc30
Hong Phuc <nguyenhongphuc98@gmail.com>
2019-03-10 18:03:02 +0700

    update, login, reigster controller. Add level mem in user

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use mysql_xdevapi\Session;
+
 
 class LoginController extends Controller
 {
@@ -34,6 +39,49 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+       // $this->middleware('guest')->except('logout');
+    }
+
+    //======================hand code==================================
+    public function showLoginForm(){
+        return view('auth/login');
+    }
+
+    public function login(Request $request){
+       // return redirect('home');
+
+        //check valite
+        $rules =[
+            'email'=>'required|email',
+            'password'=>'required|min:6'
+        ];
+
+        $message=[
+            'email.required'=>'Không thể để trong Email',
+            'email.email'=>'Tên đăng nhập phải là email',
+            'password.required'=>'Bạn chưa nhập mật khẩu',
+            'password.min'=>'Mật khẩu không được ít hơn 8 kí tự'
+        ];
+
+        $validator =Validator::make($request->all(),$rules,$message);
+
+        if($validator->fails()){
+            return redirect('login')->withErrors($validator)->withInput();
+           // return 'errr';
+        }
+        else{
+            $email=$request->input('email');
+            $pass=$request->input('password');
+
+            if(Auth::attempt(['email'=>$email, 'password'=>$pass]))
+            {
+                return redirect('home');
+            }
+            else
+            {
+                \Illuminate\Support\Facades\Session::flash('error','Vui lòng kiểm tra lại thông tin đăng nhập');
+                return redirect('login');
+            }
+        }
     }
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
@@ -48,11 +50,25 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-        return Validator::make($data, [
+        $rules =[
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
+        ];
+
+        $message =[
+            'name.required' => 'Họ và tên là trường bắt buộc',
+            'name.max' => 'Họ và tên không quá 255 ký tự',
+            'name.string'=>'Tên phải là chữ',
+            'email.required' => 'Email là trường bắt buộc',
+            'email.email' => 'Email không đúng định dạng',
+            'email.max' => 'Email không quá 255 ký tự',
+            'email.unique' => 'Email đã tồn tại',
+            'password.required' => 'Mật khẩu là trường bắt buộc',
+            'password.min' => 'Mật khẩu phải chứa ít nhất 8 ký tự',
+            'password.confirmed' => 'Xác nhận mật khẩu không đúng',
+        ];
+        return Validator::make($data,$rules,$message);
     }
 
     /**
@@ -67,6 +83,35 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'level' => '2', //2 mem - 1 duyet bai - 0 admin full control
         ]);
+    }
+
+
+    //load form singin
+    public function showRegistrationForm(){
+        return view('auth/register');
+    }
+
+    //submit form
+    public function register(Request $request){
+        $validator =$this->validator($request->all());
+
+        if($validator->fails()){
+            return redirect('register')->withErrors($validator)->withInput();
+        }
+        else
+         {
+             if($this->create($request->all()))
+             {
+                   // Session::flash('success','Đăng ký thành công');
+                    return redirect('login');
+             }
+             else
+             {
+                   // Session::flash('error','Đăng ký thất bại');
+                    return redirect('register');
+             }
+          }
     }
 }
