48bc035495350f14463e7cb6edc7c6f04d3ec66e
Thinh NND <16521179@gm.uit.edu.vn>
2019-03-11 10:59:08 +0700

    Add index.blade.php, bootstrap and fontawesome

---
+++ resources/views/index.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/fontawesome-free-5.7.2-web/css/all.css">
+
+    <link rel="stylesheet" href="../assets/bootstrap/bootstrap-grid.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Quicksand|Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../assets/styles.css">
+    <title>Ban học tập công nghệ phần mềm</title>
+</head>
+
+<body>
+    <header>
+        <nav class="nav-top">
+            <div class="wraper">
+
+                <ul class="nav-full">
+                    <li class="nav-branch ">
+                        <a class="logo" href="#">
+                            <img src="../img/bht-logo.png">
+                        </a>
+                        Sharing is Learning
+                    </li>
+                    <li class="search-area">
+                        <form type="submit">
+                            <input id="search-bar" name="search-bar" type="text" placeholder="Nhập nội dung cần tìm">
+                        </form>
+                        <i id="search-icon" class="fas fa-search"></i>
+                    </li>
+
+                    <li class="menu-items">
+                        <a class="first-item" href="#">Trang chủ</a>
+                        <a href="#">Tài liệu</a>
+                        <a href="#">Bài viết</a>
+                        <a href="#">Hỏi đáp</a>
+                        <a href="#">BXH</a>
+                    </li>
+                    <li class="account">
+                        <a href="#">Đăng nhập</a> <span>/</span>
+                        <a href="#">Đăng ký</a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+        <nav class="nav-bot">
+            <ul class="tag-list">
+                <li class="tag"><a href="#">OOP</a></li>
+                <li class="tag"><a href="#">Cơ sở dữ liệu</a></li>
+                <li class="tag"><a href="#">Mạng máy tính</a></li>
+                <li class="tag"><a href="#">Nhập môn lập trình</a></li>
+                <li class="tag"><a href="#">Kiến trúc máy tính</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="wraper">
+        <div class="row body-page">
+            <aside class="col-lg-2">
+                <div class="">
+
+                </div>
+            </aside>
+            <main id="post-show" class="col-lg-7">
+                <div class="post-container">
+                    <div class="post-head">
+                        <div class="post-user-info">
+                            <div class="avatar"><img src="../img/avt-user-01.jpg"></div>
+                            <div class="user-name">bởi <a href="#">@ptrinhcoder</a></div>
+                            <div class="date"> từ 2 tiếng trước</div>
+                            <div class="more"><i class="fas fa-chevron-circle-down"></i></div>
+                        </div>
+                        <div class="post-title"></div>Hỏi về con trỏ hàm trong C++
+                    </div>
+                    <div class="post-body brief">Mọi người ơi trong C++ ấy, có cái khái niệm là con trỏ hàm. Em có đọc tài liệu của thầy Nguyễn Niê Đức Thịnh nhưng em vẫn chưa hiểu là vì sao chúng ta lại dùng cái đó. Và vì sao lại như vậy. Mọi người có thể xem đoạn chương trình của em ở dưới</div>
+                    <div class="post-foot">
+                        <ul class="post-tag">
+                            <li class="tag tag-dark">C++</li>
+                            <li class="tag tag-dark">Nhập môn lập trình</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="post-container">
+                        <div class="post-head">
+                            <div class="post-user-info">
+                                <div class="avatar"><img src="../img/avt-user-01.jpg"></div>
+                                <div class="user-name">bởi <a href="#">@ptrinhcoder</a></div>
+                                <div class="date"> từ 2 tiếng trước</div>
+                                <div class="more"><i class="fas fa-chevron-circle-down"></i></div>
+                            </div>
+                            <div class="post-title"></div>Hỏi về con trỏ hàm trong C++
+                        </div>
+                        <div class="post-body brief">Mọi người ơi trong C++ ấy, có cái khái niệm là con trỏ hàm. Em có đọc tài liệu của thầy Nguyễn Niê Đức Thịnh nhưng em vẫn chưa hiểu là vì sao chúng ta lại dùng cái đó. Và vì sao lại như vậy. Mọi người có thể xem đoạn chương trình của em ở dưới</div>
+                        <div class="post-foot">
+                            <ul class="post-tag">
+                                <li class="tag tag-dark">C++</li>
+                                <li class="tag tag-dark">Nhập môn lập trình</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="post-container">
+                            <div class="post-head">
+                                <div class="post-user-info">
+                                    <div class="avatar"><img src="../img/avt-user-01.jpg"></div>
+                                    <div class="user-name">bởi <a href="#">@ptrinhcoder</a></div>
+                                    <div class="date"> từ 2 tiếng trước</div>
+                                    <div class="more"><i class="fas fa-chevron-circle-down"></i></div>
+                                </div>
+                                <div class="post-title"></div>Hỏi về con trỏ hàm trong C++
+                            </div>
+                            <div class="post-body brief">Mọi người ơi trong C++ ấy, có cái khái niệm là con trỏ hàm. Em có đọc tài liệu của thầy Nguyễn Niê Đức Thịnh nhưng em vẫn chưa hiểu là vì sao chúng ta lại dùng cái đó. Và vì sao lại như vậy. Mọi người có thể xem đoạn chương trình của em ở dưới</div>
+                            <div class="post-foot">
+                                <ul class="post-tag">
+                                    <li class="tag tag-dark">C++</li>
+                                    <li class="tag tag-dark">Nhập môn lập trình</li>
+                                </ul>
+                            </div>
+                        </div>
+            </main>
+            <aside class="col-lg-3">
+                <div class="news-board">
+                    <div class="news-board-head">
+                        <h3 class="title">Tin tức</h3>
+                    </div>
+                </div>
+            </aside>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
