9f73d562997df4a008f496f102df717285fc5baf
Thinh NND <16521179@gm.uit.edu.vn>
2019-03-17 10:58:25 +0700

    add post page and login not done yet

---
+++ resources/views/login.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/fontawesome-free-5.7.2-web/css/all.css">
+
+    <link rel="stylesheet" href="../assets/bootstrap/bootstrap-grid.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Quicksand|Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../assets/styles.css">
+
+    <title>Ban học tập công nghệ phần mềm</title>
+</head>
+
+<body>
+    <header>
+        <nav class="nav-top">
+            <div class="wraper">
+                <ul class="nav-full">
+                    <li class="nav-branch ">
+                        <a class="logo" href="#">
+                            <img src="../img/bht-logo.png">
+                        </a>
+                        Sharing is Learning
+                    </li>
+                    <li class="search-area">
+                        <form type="submit">
+                            <input id="search-bar" name="search-bar" type="text" placeholder="Nhập nội dung cần tìm">
+                        </form>
+                        <i id="search-icon" class="fas fa-search"></i>
+                    </li>
+
+                    <li class="menu-items">
+                        <a class="first-item" href="#">Trang chủ</a>
+                        <a href="#">Tài liệu</a>
+                        <a href="#">Bài viết</a>
+                        <a href="#">Hỏi đáp</a>
+                        <a href="#">BXH</a>
+                    </li>
+                    <li class="account">
+                        <a href="#">Đăng nhập</a> <span>/</span>
+                        <a href="#">Đăng ký</a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+        <!-- <nav class="nav-bot">
+            <ul class="tag-list">
+                <li class="tag"><a href="#">OOP</a></li>
+                <li class="tag"><a href="#">Cơ sở dữ liệu</a></li>
+                <li class="tag"><a href="#">Mạng máy tính</a></li>
+                <li class="tag"><a href="#">Nhập môn lập trình</a></li>
+                <li class="tag"><a href="#">Kiến trúc máy tính</a></li>
+            </ul>
+        </nav> -->
+    </header>
+    <main class="login-page">
+        <div class="nb-card nb-card-login">
+            <div class="nb-card-head">
+                <h3>Đăng nhập</h3>
+            </div>
+            <div class="nb-card-body">
+                <form class="login-form">
+                    <input type="email" placeholder="email">
+                    <input type="password" placeholder="password">
+                    <input type="submit" placeholder="Đăng nhập"/>
+                </form>
+            </div>
+            <div>
+                <div class="nb-card-foot">
+                    <h5>Chưa có tài khoản? <a href=#>Đăng ký</a> </h5>
+                    
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
---
+++ resources/views/post.blade.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/fontawesome-free-5.7.2-web/css/all.css">
+
+    <link rel="stylesheet" href="../assets/bootstrap/bootstrap-grid.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Quicksand|Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../assets/styles.css">
+    <title>Ban học tập công nghệ phần mềm</title>
+</head>
+
+<body>
+    <header>
+        <nav class="nav-top">
+            <div class="wraper">
+                <ul class="nav-full">
+                    <li class="nav-branch ">
+                        <a class="logo" href="#">
+                            <img src="../img/bht-logo.png">
+                        </a>
+                        Sharing is Learning
+                    </li>
+                    <li class="search-area">
+                        <form type="submit">
+                            <input id="search-bar" name="search-bar" type="text" placeholder="Nhập nội dung cần tìm">
+                        </form>
+                        <i id="search-icon" class="fas fa-search"></i>
+                    </li>
+
+                    <li class="menu-items">
+                        <a class="first-item" href="#">Trang chủ</a>
+                        <a href="#">Tài liệu</a>
+                        <a href="#">Bài viết</a>
+                        <a href="#">Hỏi đáp</a>
+                        <a href="#">BXH</a>
+                    </li>
+                    <li class="account">
+                        <a href="#">Đăng nhập</a> <span>/</span>
+                        <a href="#">Đăng ký</a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+        <!-- <nav class="nav-bot">
+            <ul class="tag-list">
+                <li class="tag"><a href="#">OOP</a></li>
+                <li class="tag"><a href="#">Cơ sở dữ liệu</a></li>
+                <li class="tag"><a href="#">Mạng máy tính</a></li>
+                <li class="tag"><a href="#">Nhập môn lập trình</a></li>
+                <li class="tag"><a href="#">Kiến trúc máy tính</a></li>
+            </ul>
+        </nav> -->
+    </header>
+    <main class="post-page wraper">
+        <div class="row">
+            <div class="col-lg-2">
+
+            </div>
+            <div class="col-lg-7">
+
+                <div class="nb-card">
+                    <div class="nb-card-head">
+                        <div class="post-user-info">
+                            <div class="avatar">
+                                <img src="../img/avt-user-01.jpg" />
+                            </div>
+                            <div class="user-name">bởi <a href="#">@ptrinhcoder</a></div>
+                            <div class="date"> từ 2 tiếng trước</div>
+                            <div class="more"><i class="fas fa-chevron-circle-down"></i></div>
+                        </div>
+                        <h3 class="post-title">Những framework front-end phổ biến hiện nay</h3>
+                    </div>
+                    <div class="nb-card-body">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore at voluptas perferendis quod
+                            dignissimos eaque eum ullam nesciunt vero neque. Repudiandae pariatur perspiciatis accusamus
+                            id,
+                            recusandae, fugit corrupti reprehenderit ratione totam natus alias eligendi veritatis,
+                            numquam</p>
+                        <div class="post-img">
+                            <img
+                                src="https://techmaster.vn/fileman/Uploads/users/188/materialize_banner_730x234_2x.jpg" />
+                            <p>Description</p>
+                        </div>
+
+                        <p>
+                            iste quos mollitia harum vero. Nam iure veritatis dolores culpa, fuga corporis id obcaecati?
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus debitis, ab obcaecati
+                            nemo aut quia alias rem expedita voluptates amet, enim consequatur voluptatum. Odit
+                            reiciendis incidunt minima repellat quia. Odio.
+                        </p>
+                        <div class="post-img">
+                            <img
+                                src="https://techmaster.vn/fileman/Uploads/users/188/front_end_frameworks_trend_comparison_730x376_2x.jpg" />
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam velit adipisci quas tempore
+                            reprehenderit magnam, ut dicta, cum reiciendis tenetur sit mollitia placeat deleniti ipsam
+                            vel.
+                            Consectetur error magni consequuntur aperiam aspernatur. Sunt ab necessitatibus ducimus,
+                            iure
+                            iste, deserunt id omnis cumque eius eos autem commodi recusandae repudiandae quam.
+                            Temporibus
+                            esse quo quod recusandae sed. Illo reiciendis, ratione, repudiandae nobis excepturi a soluta
+                            odit obcaecati quam voluptates totam esse placeat, debitis quo possimus harum? Voluptatem
+                            error
+                            recusandae labore culpa eos.
+                        </p>
+                    </div>
+                    <div class="nb-card-foot border-top">
+                        <div class="author-name"></div>
+                        <div>
+                            <ul class="post-tag">
+                                <li class="tag tag-dark">Frontends</li>
+                                <li class="tag tag-dark">Web Dev</li>
+                            </ul>
+                        </div>
+                        <div class="interaction">
+                            <div class="rates">
+                                <i class="fas fa-heart"></i>
+                                <i class="fas fa-heart-broken"></i>
+                            </div>
+                            <div class="share">
+                                <i class="fab fa-facebook-square fb-icon"></i>
+                                <i class="fab fa-twitter tw-icon"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="nb-card">
+                    <div>
+                        <p>Discussion</p>
+                    </div>
+                    <div class="nb-card">
+                        <div class="nb-card-body">
+                            fdsafds
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
