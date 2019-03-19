252811d3e42622271605ee831ee0bf85ee803f54
Hong Phuc <nguyenhongphuc98@gmail.com>
2019-03-19 16:11:23 +0700

    change csrf

--- resources/views/create_post.blade.php
+++ resources/views/create_post.blade.php
@@ -5,7 +5,8 @@
 </head>
 <body>
 	<form method="post" action="http://website-bht.herokuapp.com/posts/CreateNewPost">
-        {{ csrf_field() }}
+        {{--{{ csrf_field() }}--}}
+		<input type="hidden" name="_token" value="{{ csrf_token() }}">
 		<input type="text" name="title" placeholder="title">
 		<input type="text" name="creator" placeholder="creator">
 		<input type="text" name="description" placeholder="description">
